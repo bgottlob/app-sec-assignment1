d9aee021804debac668eb5ea0dfe1c13543c218d
Brandon Gottlob <info@bgottlob.com>
2020-06-03 21:23:59 -0400

    Create inital main function

---
+++ spell.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  const char *corpus = NULL;
+  const char *dictionary = NULL;
+  if (argc != 3) {
+    printf("Usage: spell_check <corpus_file> <dictionary_file>\n");
+    exit(EXIT_FAILURE);
+  } else {
+    corpus = argv[1];
+    dictionary = argv[2];
+    printf("Checking `%s` against corpus `%s`\n", dictionary, corpus);
+  }
+}
